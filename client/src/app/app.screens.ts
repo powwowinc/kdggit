91dec3db5b077631f93a26dc977a435682578a31
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-06-25 11:50:44 +0000

    [Studio] Add screen "CategoryListView"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,14 +1,19 @@
 /***  Generated file, do not change.  */
 import { MenuComponent } from './menu/menu';
 import { login_PhonePortrait } from '../pages/login/PhonePortrait/login';
+import { categorylistview_PhonePortrait } from '../pages/categorylistview/PhonePortrait/categorylistview';
 export class Screens {
   static declarations = [
     MenuComponent,
-    login_PhonePortrait
+    login_PhonePortrait,
+    categorylistview_PhonePortrait
   ];
   static mapping = {
     'login': {
       PhonePortrait: login_PhonePortrait
+    },
+    'categorylistview': {
+      PhonePortrait: categorylistview_PhonePortrait
     }
   }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,2 +1,3 @@
+export { mock_categorylistview as categorylistview } from './categorylistview/index';
 export { mock_login as login } from './login/index';
 export { mock_menu as menu } from './menu/index';
